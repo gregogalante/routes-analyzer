51942d544141e673eb9c9681d91a7a2cffe7b1d3
Gregorio Galante <me@gregoriogalante.com>
2025-06-19 11:13:40 +0200

    Fix linter

--- test/dummy/config/initializers/routes_analyzer.rb
+++ test/dummy/config/initializers/routes_analyzer.rb
@@ -8,12 +8,12 @@ Routes::Analyzer.configure do |config|
   #   - redis://localhost:6379/0
   #   - redis://username:password@localhost:6379/0
   #   - rediss://localhost:6380/0 (for SSL/TLS)
-  config.redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/0'
-  
+  config.redis_url = ENV["REDIS_URL"] || "redis://localhost:6379/0"
+
   # Redis key prefix for storing route usage data
   # This helps avoid conflicts with other Redis data in your application
-  config.redis_key_prefix = 'routes_analyzer'
-  
+  config.redis_key_prefix = "routes_analyzer"
+
   # Timeframe for route usage analysis in days
   # Only routes accessed within this timeframe will be considered "active"
   # This also determines how long data is stored in Redis
